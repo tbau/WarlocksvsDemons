8c9dc5504cc879e5b6ea7e0685508e424a44df53
Thomas <tbau@my.apsu.edu>
2017-04-01 16:29:01 -0500

    Initial commit

---
+++ app/src/main/java/com/bauandhornick/warlocksvsdemons/BattleFieldView.java
@@ -0,0 +1,23 @@
+package com.bauandhornick.warlocksvsdemons;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Thomas on 3/24/2017.
+ */
+
+public class BattleFieldView extends View {
+    public BattleFieldView(Context context) {
+        super(context);
+    }
+
+    public BattleFieldView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BattleFieldView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
