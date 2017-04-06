dddaa1318e1644943b00c35588af7009f4f14f4b
Alex Hornick <tennalexd@yahoo.com>
2017-04-06 16:24:10 -0500

    Creating Weapon class, Ally fixes

--- app/src/main/java/com/bauandhornick/warlocksvsdemons/Ally.java
+++ app/src/main/java/com/bauandhornick/warlocksvsdemons/Ally.java
@@ -7,69 +7,24 @@ import android.graphics.Bitmap;
  */
 
 public class Ally extends Character {
-    private int typeOfWeapon;
-    private int weaponRange;
-    private int weaponAffinity;
-    private int weaponSpeed;
-    private double rechargeRate;
+    Weapon weapon;
+    private int costToBuy;
+
 
     public Ally(int pos_x, int pos_y, int width, int height, int damage, int requireFlip,
-                Bitmap appearance, String weakness, String affinity, int typeOfWeapon,
-                int weaponRange, int weaponAffinity, int weaponSpeed, double rechargeRate,
-                int costToBuy, String areaOfEffect) {
+                Bitmap appearance, String weakness, String affinity, int costToBuy, Weapon weapon) {
         super(pos_x, pos_y, width, height, damage, requireFlip, appearance, weakness, affinity);
-        this.typeOfWeapon = typeOfWeapon;
+        this.weapon = weapon;
+        /*this.typeOfWeapon = typeOfWeapon;
         this.weaponRange = weaponRange;
         this.weaponAffinity = weaponAffinity;
         this.weaponSpeed = weaponSpeed;
-        this.rechargeRate = rechargeRate;
+        this.rechargeRate = rechargeRate;*/
         this.costToBuy = costToBuy;
-        this.areaOfEffect = areaOfEffect;
+        //this.areaOfEffect = areaOfEffect;
     }
 
-    private int costToBuy;
-    private String areaOfEffect;
 
-
-    public int getTypeOfWeapon() {
-        return typeOfWeapon;
-    }
-
-    public void setTypeOfWeapon(int typeOfWeapon) {
-        this.typeOfWeapon = typeOfWeapon;
-    }
-
-    public int getWeaponRange() {
-        return weaponRange;
-    }
-
-    public void setWeaponRange(int weaponRange) {
-        this.weaponRange = weaponRange;
-    }
-
-    public int getWeaponAffinity() {
-        return weaponAffinity;
-    }
-
-    public void setWeaponAffinity(int weaponAffinity) {
-        this.weaponAffinity = weaponAffinity;
-    }
-
-    public int getWeaponSpeed() {
-        return weaponSpeed;
-    }
-
-    public void setWeaponSpeed(int weaponSpeed) {
-        this.weaponSpeed = weaponSpeed;
-    }
-
-    public double getRechargeRate() {
-        return rechargeRate;
-    }
-
-    public void setRechargeRate(double rechargeRate) {
-        this.rechargeRate = rechargeRate;
-    }
 
     public int getCostToBuy() {
         return costToBuy;
@@ -79,12 +34,6 @@ public class Ally extends Character {
         this.costToBuy = costToBuy;
     }
 
-    public String getAreaOfEffect() {
-        return areaOfEffect;
-    }
-
-    public void setAreaOfEffect(String areaOfEffect) {
-        this.areaOfEffect = areaOfEffect;
-    }
+   
 
 }
---
+++ app/src/main/java/com/bauandhornick/warlocksvsdemons/Weapon.java
@@ -0,0 +1,82 @@
+package com.bauandhornick.warlocksvsdemons;
+
+/**
+ * Created by Alex Hornick on 4/6/2017.
+ */
+
+public class Weapon {
+
+
+    private int typeOfWeapon;
+    private int weaponRange;
+    private int weaponAffinity;
+    private int weaponSpeed;
+    private double rechargeRate;
+    private String areaOfEffect;
+
+    public Weapon(int typeOfWeapon, int weaponRange, int weaponAffinity, int weaponSpeed, double rechargeRate, String areaOfEffect) {
+        this.typeOfWeapon = typeOfWeapon;
+        this.weaponRange = weaponRange;
+        this.weaponAffinity = weaponAffinity;
+        this.weaponSpeed = weaponSpeed;
+        this.rechargeRate = rechargeRate;
+        this.areaOfEffect = areaOfEffect;
+    }
+
+    public int getTypeOfWeapon() {
+        return typeOfWeapon;
+    }
+
+    public void setTypeOfWeapon(int typeOfWeapon) {
+        this.typeOfWeapon = typeOfWeapon;
+    }
+
+    public int getWeaponRange() {
+        return weaponRange;
+    }
+
+    public void setWeaponRange(int weaponRange) {
+        this.weaponRange = weaponRange;
+    }
+
+    public int getWeaponAffinity() {
+        return weaponAffinity;
+    }
+
+    public void setWeaponAffinity(int weaponAffinity) {
+        this.weaponAffinity = weaponAffinity;
+    }
+
+    public int getWeaponSpeed() {
+        return weaponSpeed;
+    }
+
+    public void setWeaponSpeed(int weaponSpeed) {
+        this.weaponSpeed = weaponSpeed;
+    }
+
+    public double getRechargeRate() {
+        return rechargeRate;
+    }
+
+    public void setRechargeRate(double rechargeRate) {
+        this.rechargeRate = rechargeRate;
+    }
+
+    public int getCostToBuy() {
+        return costToBuy;
+    }
+
+    public void setCostToBuy(int costToBuy) {
+        this.costToBuy = costToBuy;
+    }
+
+    public String getAreaOfEffect() {
+        return areaOfEffect;
+    }
+
+    public void setAreaOfEffect(String areaOfEffect) {
+        this.areaOfEffect = areaOfEffect;
+    }
+
+}
